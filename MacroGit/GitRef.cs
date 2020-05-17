1d506fb840a9f1ab3466332a5771935e96f091c3
Ron MacNeil <macro@hotmail.com>
2020-05-17 23:51:23 +1000

    Add IsBranch and IsTag to GitRef

--- MacroGit/GitRef.cs
+++ MacroGit/GitRef.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using MacroGuards;
 
@@ -19,12 +20,16 @@ namespace MacroGit
             FullName = fullName;
             Name = new GitRefNameComponent(fullName.ToString().Split('/').Last());
             Target = target;
+            IsBranch = FullName.ToString().StartsWith("refs/heads/", StringComparison.Ordinal);
+            IsTag = FullName.ToString().StartsWith("refs/tags/", StringComparison.Ordinal);
         }
 
 
         public GitRefNameComponent Name { get; }
         public GitRefName FullName { get; }
         public GitSha1 Target { get; }
+        public bool IsBranch { get; }
+        public bool IsTag { get; }
 
     }
 }
